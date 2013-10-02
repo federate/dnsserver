355b19e36b33f91288e6c7523b98e3c51704e0ec
klarrimore <klarrimore@icehook.com>
2013-10-02 06:08:09 -0400

    cleaned up config

--- lib/dnsserver.rb
+++ lib/dnsserver.rb
@@ -12,39 +12,60 @@ require 'dnsserver/config'
 
 module DNSServer
 
-  INTERFACES = [
-    [:udp, "0.0.0.0", 5300],
-    [:tcp, "0.0.0.0", 5300]
-  ]
-
   Name = Resolv::DNS::Name
   IN = Resolv::DNS::Resource::IN
 
-  GOOGLE_RESOLVER = RubyDNS::Resolver.new([[:udp, "8.8.8.8", 53], [:udp, "8.8.4.4", 53]])
-  TELASTIC_RESOLVER = RubyDNS::Resolver.new([[:udp, "184.173.103.52", 53]])
-
   module ClassMethods
 
-    attr_accessor :config
+    attr_accessor :config_file, :environment, :logger
+
+    def config
+      DNSServer::Config
+    end
+
+    def interfaces
+      self.config.server.interfaces.collect { |int| [int['protocol'].to_sym, int['addr'], int['port'].to_i] }
+    end
+
+    def resolvers
+      @resolvers ||= self.config.resolvers.collect do |resolver|
+        {
+          :name => resolver['name'],
+          :resolver => RubyDNS::Resolver.new(resolver.servers.collect { |server|
+                                             [server['protocol'].to_sym, server['addr'], server['port'].to_i]
+                                            })
+        }
+      end
+    end
+
+    def resolver(name)
+      r = self.resolvers.find { |resolver| resolver[:name] == name }
+      r[:resolver]
+    end
 
     def run
-      RubyDNS::run_server(:listen => INTERFACES) do
+      RubyDNS::run_server(:listen => self.interfaces) do
         match(/e164.arpa$/) do |transaction|
-          transaction.passthrough!(TELASTIC_RESOLVER)
+          transaction.passthrough!(DNSServer.resolver('telaris'))
         end
 
         # Default DNS handler
         otherwise do |transaction|
-          transaction.passthrough(GOOGLE_RESOLVER)
+          transaction.passthrough!(DNSServer.resolver('google'))
         end
       end
     end
 
-    def init(config_file = nil)
+    def init(config_file = nil, environment = 'development')
+      @environment = environment
+      @logger ||= Logger.new(STDOUT)
+
       if config_file
         @config_file = Pathname.new config_file
         DNSServer::Config.source @config_file.realpath
       end
+
+      DNSServer::Config.namespace @environment
     end
 
   end
